2d38cab75a89f2a421ab9e6270f34b406e705dfe
1ntrepid MAXIMUM+ <blackmathsavior@gmail.com>
2019-04-21 11:50:34 -0700

    [1-2] Label - Showing Text to the Player

--- LoonyLips_31/Scripts/LoonyLips.gd
+++ LoonyLips_31/Scripts/LoonyLips.gd
@@ -2,5 +2,6 @@ extends Control
 
 func _ready():
 	var prompts = ["flightless bird", "winging it", "roosters", "cocks"]
-	var story = "Once there was a giant %s. He was always being encouraged to try but he just got tired of %s. This banter didn't go over well with the %s, but what could he expect froma bunch of %s!"
-	print(story % prompts)
+	var story = "Once there was a giant %s. He was always being encouraged to try but he just got tired of %s. This banter didn't go over well with the %s, but what could he expect from a bunch of %s!"
+	
+	$DisplayText.text = story % prompts
