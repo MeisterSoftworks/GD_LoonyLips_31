2f0389bbe72ae1c9f6c89686ebd268e732e788d3
1ntrepid MAXIMUM+ <blackmathsavior@gmail.com>
2019-04-22 12:58:12 -0700

    [1-4] Buttons!

--- LoonyLips_31/Scripts/LoonyLips.gd
+++ LoonyLips_31/Scripts/LoonyLips.gd
@@ -9,6 +9,11 @@ func _ready():
 func _on_PlayerText_text_entered(new_text):
 	update_DisplayText(new_text)
 
+func _on_TextureButton_pressed():
+	var words = $VBoxContainer/HBoxContainer/PlayerText.text
+	update_DisplayText(words)
+
 func update_DisplayText(words):
 	$VBoxContainer/DisplayText.text = words
-	$VBoxContainer/PlayerText.clear()
+	$VBoxContainer/HBoxContainer/PlayerText.clear()
+	$VBoxContainer/HBoxContainer/PlayerText.grab_focus()
