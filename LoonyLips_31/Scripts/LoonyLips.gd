0abd0677dc4b1c3d28981a460445125d9baa8a4a
1ntrepid MAXIMUM+ <blackmathsavior@gmail.com>
2019-04-22 11:17:27 -0700

    [1-3] LineEdit - Getting Text From the Player

--- LoonyLips_31/Scripts/LoonyLips.gd
+++ LoonyLips_31/Scripts/LoonyLips.gd
@@ -4,4 +4,11 @@ func _ready():
 	var prompts = ["flightless bird", "winging it", "roosters", "cocks"]
 	var story = "Once there was a giant %s. He was always being encouraged to try but he just got tired of %s. This banter didn't go over well with the %s, but what could he expect from a bunch of %s!"
 	
-	$DisplayText.text = story % prompts
+	$VBoxContainer/DisplayText.text = story % prompts
+
+func _on_PlayerText_text_entered(new_text):
+	update_DisplayText(new_text)
+
+func update_DisplayText(words):
+	$VBoxContainer/DisplayText.text = words
+	$VBoxContainer/PlayerText.clear()
